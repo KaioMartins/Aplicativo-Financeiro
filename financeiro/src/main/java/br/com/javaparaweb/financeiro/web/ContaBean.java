cb4d53ec1d89a4e209ea2594d50a65eda3f41bd0
Kaio Martins <kaio.ccmartins@gmail.com>
2016-08-13 11:48:15 -0300

    Criação de Relatórios com iReport - parte 1
    Criação de relatórios da listagem de contas de usuário
    - Pdf
    - Html
    - Excel
    - OpenOffice

--- financeiro/src/main/java/br/com/javaparaweb/financeiro/web/ContaBean.java
+++ financeiro/src/main/java/br/com/javaparaweb/financeiro/web/ContaBean.java
@@ -5,6 +5,12 @@ import java.util.List;
 import javax.faces.bean.*;
 import br.com.javaparaweb.financeiro.conta.Conta;
 import br.com.javaparaweb.financeiro.conta.ContaRN;
+import org.primefaces.model.StreamedContent;
+import javax.faces.context.FacesContext;
+import javax.faces.application.FacesMessage;
+import br.com.javaparaweb.financeiro.web.util.RelatorioUtil;
+import br.com.javaparaweb.financeiro.util.UtilException;
+import java.util.HashMap;
 
 @ManagedBean
 @RequestScoped
@@ -13,6 +19,8 @@ public class ContaBean {
 	private List<Conta> lista = null;
 	@ManagedProperty(value = "#{contextoBean}")
 	private ContextoBean contextoBean;
+	private StreamedContent arquivoRetorno;
+	private int tipoRelatorio;
 
 	public String salvar() {
 		this.selecionada.setUsuario(this.contextoBean.getUsuarioLogado());
@@ -38,6 +46,33 @@ public class ContaBean {
 		return null;
 	}
 
+	public StreamedContent getArquivoRetorno(){
+		FacesContext context = FacesContext.getCurrentInstance();
+		String usuario = contextoBean.getUsuarioLogado().getLogin();
+		String nomeRelatorioJasper = "contas";
+		String nomeRelatorioSaida = usuario + "_contas";
+		RelatorioUtil relatorioUtil = new RelatorioUtil();
+		HashMap parametrosRelatorio = new HashMap();
+		parametrosRelatorio.put("codigoUsuario", 
+				contextoBean.getUsuarioLogado().getCodigo());
+		try {
+			this.arquivoRetorno = relatorioUtil.geraRelatorio(parametrosRelatorio, nomeRelatorioJasper, 
+					nomeRelatorioSaida, this.tipoRelatorio);
+		} catch (UtilException e) {
+			context.addMessage(null, new FacesMessage(e.getMessage()));
+			return null;
+		}
+		return this.arquivoRetorno;
+	}
+	
+	public int getTipoRelatorio(){
+		return tipoRelatorio;
+	}
+	
+	public void setTipoRelatorio(int tipoRelatorio){
+		this.tipoRelatorio = tipoRelatorio;
+	}
+	
 	public Conta getSelecionada() {
 		return selecionada;
 	}
